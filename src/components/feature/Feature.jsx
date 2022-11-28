7fb838e6ab1d1edc414d4066f12f1bdf479a7c97
viniciusBressiani <vi_bio@hotmail.com>
2022-11-28 20:59:03 -0300

    Done what is escm with jsx and css

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
+const Feature = ({text,title}) => {
   return (
-    <div>
-      Feature
+    <div className="escm__features-container__feature">
+       <div className="escm__features-container__feature-title">
+          <div />
+          <h2>{title}</h2>
+       </div>
+       <div className="escm__features-container__feature-text">
+        <p>{text}</p>
+       </div>
     </div>
   )
 }
--- src/containers/whatESCM/WhatESCM.jsx
+++ src/containers/whatESCM/WhatESCM.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './whatescm.css';
+import Feature from './../../components/feature/Feature';
 
 const WhatESCM = () => {
   return (
-    <div>
-      WhatESCM
+    <div className="escm__whatescm section__margin" id='whatescm'>
+      <div className="escm__whatescm-feature">
+        <Feature 
+          title='What is GPT-3'
+          text= 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+      <div className="escm__whatescm-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="escm__whatescm-container">
+      <Feature 
+          title='Chatbots'
+          text= 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. '/>
+      <Feature 
+          title='Knowledgebase'
+          text= 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'/>
+      <Feature 
+          title='Education'
+          text= 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'/>
+      </div>
     </div>
   )
 }
