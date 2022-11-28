13d453c3fc5802bc8d3217390584d44fbfd12f13
viniciusBressiani <vi_bio@hotmail.com>
2022-11-28 10:23:26 -0300

    Done brands with jsx and css

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './brand.css';
+import {google,shopify,slack,dropbox,atlassian} from './import.js';
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='escm__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={atlassian} alt="google" />
+      </div>
     </div>
   )
 }
