772e31c3c7a093a14a3d43c5efbe4cef99b1962a
viniciusBressiani <vi_bio@hotmail.com>
2022-11-30 07:36:27 -0300

    Done Cta with css and jsx

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const Cta = () => {
   return (
-    <div>
-      Cta
+    <div className="escm__cta">
+      <div className="escm__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="escm__cta-button">
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
