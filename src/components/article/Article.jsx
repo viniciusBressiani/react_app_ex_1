5e157fcd445351dc06d4569afd77a592cbf2f852
viniciusBressiani <vi_bio@hotmail.com>
2022-11-30 17:44:38 -0300

    The blog it's done with jsx and css

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({title, image,date}) => {
+
+  
+  
   return (
-    <div>
-      Article
+    <div className="escm__article">
+      <div className="escm__article-image">
+        <img src={image} alt="" />
+      </div>
+      <div className="escm__article-content">
+        <div >
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+          <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import './blog.css';
 
+import { Article } from '../../components';
+import {blog01,blog02,blog03,blog04,blog05} from './import';
+
+const articlesData = [
+  {
+    date: "Sep 26, 2021",
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    image: blog01
+  },
+  {
+    date: "Sep 26, 2021",
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    image: blog02
+  },
+  {
+    date: "Sep 26, 2021",
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    image: blog03
+  },
+  {
+    date: "Sep 26, 2021",
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    image: blog04
+  },
+  {
+    date: "Sep 26, 2021",
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    image: blog05
+  },
+]
+
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className="escm__blog section__padding" id='blog'>
+      <div className="escm__blog-heading ">
+        <h2 className='gradient__text'>A lot is happening, 
+            We are blogging about it.</h2>
+      </div>
+      <div className="escm__blog-container">
+        <div className="escm__blog-container__groupA">
+          <Article title={articlesData[0].title} image= {articlesData[0].image} date= {articlesData[0].date} />
+        </div>
+        <div className="escm__blog-container__groupB">
+            <Article title={articlesData[1].title} image= {articlesData[1].image} date= {articlesData[1].date}/>
+            <Article title={articlesData[2].title} image= {articlesData[2].image} date= {articlesData[2].date}/>   
+            <Article title={articlesData[3].title} image= {articlesData[3].image} date= {articlesData[3].date}/>
+            <Article title={articlesData[4].title} image= {articlesData[4].image} date= {articlesData[4].date}/>
+        </div>
+      </div>
     </div>
   )
 }
