b862d708bb5ecb1ab310d7eb2283643a4e2da3eb
viniciusBressiani <vi_bio@hotmail.com>
2022-11-30 20:53:40 -0300

    The footer is done with jsx and css

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import './footer.css';
+import escmLogo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="escm__footer section__padding">
+      <div className="escm__footer-heading">
+        <h2 className="gradient__text">Do you want to step in to the future before others</h2>
+      </div>
+      <div className="escm__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="escm__footer-links">
+        <div className="escm__footer-links_logo">
+          <img src={escmLogo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="escm__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="escm__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="escm__footer-links_div">
+          <h4>Links</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="escm__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
